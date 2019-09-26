022e9e08d0dae5ee16c3f26d9535476f98677cac
mandybandy <mandl150034@sr.htlweiz.at>
2019-09-26 08:24:35 +0200

    initial commit

---
+++ src/asmcapture.h
@@ -0,0 +1,24 @@
+/*
+
+*/
+
+#ifndef asmcapture_h
+#define asmcapture_h
+
+
+
+
+#include <Arduino.h>
+
+class asm {
+public:
+
+	asm(); //verbinden mit Schnittstelle
+	readData();
+
+private:
+
+
+};
+
+#endif //asmcapture.h
