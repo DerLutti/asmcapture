d596a31548bbd7079cb429989af5f4348c72a6ca
Florian Luttenberger <lukas.kroell@evon-smarthome.com>
2019-10-17 08:28:56 +0200

    small bugfix

--- src/rapidjsontest.cpp
+++ src/rapidjsontest.cpp
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <list>
+#include <string>
 #include "rapidjson/writer.h" /*JSON write*/
 #include "rapidjson/reader.h" /*JSON read*/
 #include "rapidjson/stringbuffer.h" /*JSON buffer*/
 
 using namespace rapidjson;
+using namespace std;
 
-const char* json = "{
-"hello": "world",
-"t" : true,
-"f" : false,
-"n" : null,
-"i" : 123,
-"pi" : 3.1416,
-"a" : [1, 2, 3, 4]
-}"
+string json = "{\"hello\": \"world\",\"t\" : true,\"f\" : false,\"n\" : null,\"i\" : 123,\"pi\" : 3.1416,\"a\" : [1, 2, 3, 4]}"
 
 int main() 
 {
