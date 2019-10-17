bec2be25bf05b33e7767564af74a77bdb78ae4f7
Florian Luttenberger <lukas.kroell@evon-smarthome.com>
2019-10-17 08:59:44 +0200

    testing rapidjson

--- src/rapidjsontest.cpp
+++ src/rapidjsontest.cpp
@@ -12,8 +12,8 @@ using namespace std;
 const char* json = "{\"a\" : [{\"b\":123, \"c\":\"asd\"}, {\"b\":123, \"c\":\"asd\"}, {\"b\":123, \"c\":\"asd\"}, {\"b\":123, \"c\":\"asd\"}]}";
 
 struct idc {
-	int b,
-	int c
+	int b;
+	int c;
 };
 
 int main() 
@@ -21,8 +21,8 @@ int main()
 	printf(json);
 	Document doc;
 	doc.Parse(json);
-	assert(doc.isObject);
-	const Value& a = document["a"];
+	assert(doc.IsObject);
+	const Value& a = doc["a"];
 	assert(a.IsArray);
 
 	for (Value::ConstValueIterator itr = a.Begin(); itr != a.End(); ++itr)
@@ -30,8 +30,9 @@ int main()
 		const Value& attribute = *itr;
 		assert(attribute.IsObject()); // each attribute is an object
 		for (Value::ConstMemberIterator itr2 = attribute.MemberBegin(); itr2 != attribute.MemberEnd(); ++itr2) {
-			printf(itr2->name.GetString() + " : " + itr2->value.GetString());
-		}
+			printf(itr2->name.GetString());
+			printf(itr2->value.GetString());
+		} 
 	}
 
 	return 0;
